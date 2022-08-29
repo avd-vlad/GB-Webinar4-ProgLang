96ddab7e01a03c27ed3a68bbda9c7ebba966c6f5
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-29 20:46:33 +0300

    Webinar tasks completed

---
+++ Task-1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*--------------------------------------------------------------------------------------------------------------------
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+--------------------------------------------------------------------------------------------------------------------*/
+
+int NumberLength(int num)
+{
+    int cnt=0;
+    while(num > 0)
+    {
+        num/=10;
+        cnt++;
+    }
+    return cnt;
+}
+
+
+Console.Write("Please enter number to check digits quantity: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number {num} has {NumberLength(num)} digits");
+
